fdcb3d21e852ba4ad1b5cf1339492f4eb2ae76ee
Akintola-Stephen <sakintola351@gmail.com>
2024-06-22 21:26:22 +0100

    Implement middleware for  error handling and profile refactoring

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,18 +1,16 @@
 const { Profile } = require('../models');
 
-
 const getProfile = async (req, res, next) => {
-    const profileId = req.get('profile_id') || 0;
-    try {
-        const profile = await Profile.findOne({ where: { id: profileId } });
-        if (!profile) {
-            return res.status(401).end();
-        }
-        req.profile = profile;
-        next();
-    } catch (error) {
-        next(error);
+    const profileId = req.get('profile_id');
+    if (!profileId) {
+        return res.status(401).end();
     }
+    const profile = await Profile.findOne({ where: { id: profileId } });
+    if (!profile) {
+        return res.status(401).end();
+    }
+    req.profile = profile;
+    next();
 };
 
-module.exports = getProfile;
+module.exports = { getProfile };
