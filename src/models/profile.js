a19ecf0d56a607b57d4a1b9e0aa2188a6404cd6b
Akintola-Stephen <sakintola351@gmail.com>
2024-06-22 14:09:03 +0100

    Add folder structured pattern

---
+++ src/models/profile.js
@@ -0,0 +1,35 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Profile extends Model { }
+
+Profile.init(
+    {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        profession: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        balance: {
+            type: DataTypes.DECIMAL(12, 2),
+            defaultValue: 0.0
+        },
+        type: {
+            type: DataTypes.ENUM('client', 'contractor'),
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Profile'
+    }
+);
+
+module.exports = Profile;
