6e27a60acd632873ce57eb49e3eff3352555fb18
Akintola-Stephen <sakintola351@gmail.com>
2024-07-02 21:20:52 +0100

    Changes to my file

--- src/dal/balanceDAL.js
+++ src/dal/balanceDAL.js
@@ -1,6 +1,13 @@
-import { Job, Contract, Profile, sequelize  } from "../model/index.js";
+import { Job, Contract, Profile, sequelize } from "../model/index.js";
 
 
+/**
+ * Deposits a specified amount into the client's balance, ensuring it does not exceed 25% of total unpaid jobs in progress.
+ * @param {number} clientId - The ID of the client making the deposit.
+ * @param {number} amount - The amount of money to be deposited.
+ * @returns {Promise<object>} - The updated client profile with the new balance if the deposit is successful.
+ * @throws {Error} - If the client is not found or if the deposit amount exceeds the allowed limit.
+ */
 const depositToBalance = async (clientId, amount) => {
     return sequelize.transaction(async (t) => {
         const client = await Profile.findOne({ where: { id: clientId, type: 'client' } }, { transaction: t });
@@ -35,4 +42,4 @@ const depositToBalance = async (clientId, amount) => {
     });
 };
 
-export  { depositToBalance }
+export { depositToBalance }
