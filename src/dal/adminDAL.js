501dbfbda9557bb215f8020b9a860cb31b14dbbd
Akintola-Stephen <sakintola351@gmail.com>
2024-07-02 21:18:48 +0100

    updated code changes

--- src/dal/adminDAL.js
+++ src/dal/adminDAL.js
@@ -1,6 +1,12 @@
 import { Job, Contract, Profile, sequelize } from "../model/index.js";
 import { Op, fn, literal, col } from 'sequelize';
 
+/**
+ * Retrieves the profession that earned the most money within a specified date range.
+ * @param {string} start - The start date of the range to filter jobs by payment date.
+ * @param {string} end - The end date of the range to filter jobs by payment date.
+ * @returns {Promise<Object>} The profession with the highest total earnings within the specified date range.
+ */
 const getBestProfession = async (start, end) => {
     const professions = await Job.findAll({
         where: {
@@ -24,9 +30,9 @@ const getBestProfession = async (start, end) => {
         ],
         group: [col('Contract.Contractor.profession')],
         order: [[literal('total'), 'DESC']]
-    })
+    });
 
-    return professions[0]
+    return professions[0];
 };
 
 const getBestClients = async (start, end, limit) => {
@@ -57,4 +63,4 @@ const getBestClients = async (start, end, limit) => {
     return clients
 };
 
-export  { getBestProfession, getBestClients };
+export { getBestProfession, getBestClients };
